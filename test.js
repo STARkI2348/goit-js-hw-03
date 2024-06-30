ffcb1ed62956ff14ffb3e40582f1cb5a53daeb15
STARkI2348 <107957195+STARkI2348@users.noreply.github.com>
2024-06-30 20:48:56 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,14 +1,9 @@
-function calculateTotalPrice(order) {
-    let a;
-    let b;
-    for(let i = 0; i < order.length - 1; i += 2){
-        a = order[i] + order[i + 1];
-        b = order[i + 4] + order[i + 2];
-        order.push(a);
-    }
-    return order[order.length - 1];
+function calculateTotal(number) {
+  let sum = 0;
+  for (let i = 1; i <= number; i++) {
+    sum += i;
   }
-console.log(calculateTotalPrice([12, 85, 37, 4]));
-console.log(calculateTotalPrice([164, 48, 291]));
-console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
-console.log(calculateTotalPrice());
+   return sum;
+ }
+ 
+console.log(calculateTotal(18));
