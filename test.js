faa0e581b9d694cbfd934096a718d8efacdc7d85
STARkI2348 <107957195+STARkI2348@users.noreply.github.com>
2024-07-01 22:47:58 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,24 @@
-function createReversedArray(arr) {
-  return arr;
+function slugify (title) {
+  title = title.replace(/^\s+|\s+$/g, ''); // trim
+  title = title.toLowerCase();
+
+  // remove accents, swap ñ for n, etc
+  var from = "àáãäâèéëêìíïîòóöôùúüûñç·/_,:;";
+  var to   = "aaaaaeeeeiiiioooouuuunc------";
+
+  for (var i=0, l=from.length ; i<l ; i++) {
+    title = title.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+  }
+
+  title = title.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+      .replace(/\s+/g, '-') // collapse whitespace and replace by -
+      .replace(/-+/g, '-'); // collapse dashes
+
+  return title;
 }
-console.log(createReversedArray(12, 85, 37, 4));
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+
+
